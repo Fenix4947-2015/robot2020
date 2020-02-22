cc2f79522bcfc12be80aef6eda161584a74c2f17
edube <edube@cogiscan.com>
2020-02-22 14:08:44 -0500

    Changements pour SmartDashboardSettings (2)

--- robot_compet/src/main/java/frc/robot/SmartDashboardSettings.java
+++ robot_compet/src/main/java/frc/robot/SmartDashboardSettings.java
@@ -7,7 +7,7 @@ public class SmartDashboardSettings {
   private double _pidP = 0.024;
   private double _pidI = 0.0052;
   private double _pidD = 0.005;
-  private double _pidF = 1.0;
+  private double _pidF = 0.175;
   private String _pidType = "LLANGLE";
 
   public SmartDashboardSettings() {
@@ -23,10 +23,10 @@ public class SmartDashboardSettings {
   }
 
   public void refreshPidValues() {
-    _pidP = SmartDashboard.getNumber("pidP", 1.0);
-    _pidI = SmartDashboard.getNumber("pidI", 1.0);
-    _pidD = SmartDashboard.getNumber("pidD", 1.0);
-    _pidF = SmartDashboard.getNumber("pidF", 1.0);
+    _pidP = SmartDashboard.getNumber("pidP", 0.024);
+    _pidI = SmartDashboard.getNumber("pidI", 0.0052);
+    _pidD = SmartDashboard.getNumber("pidD", 0.005);
+    _pidF = SmartDashboard.getNumber("pidF", 0.175);
     _pidType = SmartDashboard.getString("pidType", "LLANGLE");
   }
 
