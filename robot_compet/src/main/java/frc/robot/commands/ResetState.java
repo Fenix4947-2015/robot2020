18133a52dd3112d6051fe8f8821e834e6ba39d91
edube <edube@cogiscan.com>
2020-03-05 16:23:02 -0500

    Debug

--- robot_compet/src/main/java/frc/robot/commands/ResetState.java
+++ robot_compet/src/main/java/frc/robot/commands/ResetState.java
@@ -24,6 +24,7 @@ public class ResetState extends CommandBase {
 
   @Override
   public void execute() {
+    System.out.println("ResetState execute()");
   }
   
   @Override
--- robot_compet/src/main/java/frc/robot/commands/pizzaTurner/RetractPizzaTurner.java
+++ robot_compet/src/main/java/frc/robot/commands/pizzaTurner/RetractPizzaTurner.java
@@ -31,6 +31,7 @@ public class RetractPizzaTurner extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    System.out.println("ResetState execute()");
   }
 
   // Called once the command ends or is interrupted.
